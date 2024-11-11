a733a225724d364aab848dc6f725514be8f62fce
Cyber-2000 <qwe2536569210@gmail.com>
2024-11-11 09:54:15 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -9,31 +9,30 @@ cd $local_folder
 trackers_list=$(wget --no-check-certificate -qO- https://trackerslist.com/all_aria2.txt)
 ariaport=$(shuf -i 13000-19000 -n 1)
 mkdir /etc/aria2/
-ariaver1=$(curl -s "https://api.github.com/repos/aria2/aria2/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-ariaver2=$(curl -s "https://api.github.com/repos/aria2/aria2/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c9-20)
-apt-get install build-essential nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev libssl-dev libuv1-dev -q -y
-curl --retry 5 -LO --progress-bar https://github.com/aria2/aria2/releases/download/${ariaver1}/aria2-${ariaver2}.tar.xz
-tar -xvf aria2*.xz
-rm *.xz
-cd /$local_folder/aria2*
-sed -i 's/"1", 1, 16/"128", 1, -1/g' /$local_folder/aria2*/src/OptionHandlerFactory.cc
-sed -i 's/"20M", 1_m, 1_g/"4K", 1_k, 1_g/g' /$local_folder/aria2*/src/OptionHandlerFactory.cc
-sed -i 's/PREF_CONNECT_TIMEOUT, TEXT_CONNECT_TIMEOUT, "60", 1, 600/PREF_CONNECT_TIMEOUT, TEXT_CONNECT_TIMEOUT, "30", 1, 600/g' /$local_folder/aria2*/src/OptionHandlerFactory.cc
-sed -i 's/PREF_PIECE_LENGTH, TEXT_PIECE_LENGTH, "1M", 1_m, 1_g/PREF_PIECE_LENGTH, TEXT_PIECE_LENGTH, "4k", 1_k, 1_g/g' /$local_folder/aria2*/src/OptionHandlerFactory.cc
-sed -i 's/new NumberOptionHandler(PREF_RETRY_WAIT, TEXT_RETRY_WAIT, "0", 0, 600/new NumberOptionHandler(PREF_RETRY_WAIT, TEXT_RETRY_WAIT, "2", 0, 600/g' /$local_folder/aria2*/src/OptionHandlerFactory.cc
-sed -i 's/new NumberOptionHandler(PREF_SPLIT, TEXT_SPLIT, "5", 1, -1,/new NumberOptionHandler(PREF_SPLIT, TEXT_SPLIT, "8", 1, -1,/g' /$local_folder/aria2*/src/OptionHandlerFactory.cc
-./configure
-make -j $(nproc --all)
-make install
-chmod +x /usr/local/bin/aria2c
-rm -rf /root/aria2*
-apt-get autoremove -y
+# ariaver1=$(curl -s "https://api.github.com/repos/aria2/aria2/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+# ariaver2=$(curl -s "https://api.github.com/repos/aria2/aria2/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c9-20)
+# apt-get install build-essential nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev libssl-dev libuv1-dev -q -y
+# curl --retry 5 -LO --progress-bar https://github.com/aria2/aria2/releases/download/${ariaver1}/aria2-${ariaver2}.tar.xz
+# tar -xvf aria2*.xz
+# rm aria2*.xz
+# cd aria2*
+# sed -i 's/"1", 1, 16/"128", 1, -1/g' src/OptionHandlerFactory.cc
+# #sed -i 's/"20M", 1_m, 1_g/"4K", 1_k, 1_g/g' src/OptionHandlerFactory.cc
+# #sed -i 's/PREF_CONNECT_TIMEOUT, TEXT_CONNECT_TIMEOUT, "60", 1, 600/PREF_CONNECT_TIMEOUT, TEXT_CONNECT_TIMEOUT, "30", 1, 600/g' src/OptionHandlerFactory.cc
+# #sed -i 's/PREF_PIECE_LENGTH, TEXT_PIECE_LENGTH, "1M", 1_m, 1_g/PREF_PIECE_LENGTH, TEXT_PIECE_LENGTH, "4k", 1_k, 1_g/g' src/OptionHandlerFactory.cc
+# #sed -i 's/new NumberOptionHandler(PREF_RETRY_WAIT, TEXT_RETRY_WAIT, "0", 0, 600/new NumberOptionHandler(PREF_RETRY_WAIT, TEXT_RETRY_WAIT, "2", 0, 600/g' src/OptionHandlerFactory.cc
+# #sed -i 's/new NumberOptionHandler(PREF_SPLIT, TEXT_SPLIT, "5", 1, -1,/new NumberOptionHandler(PREF_SPLIT, TEXT_SPLIT, "8", 1, -1,/g' src/OptionHandlerFactory.cc
+# ./configure
+# make -j $(nproc --all)
+# make install
+# chmod +x /usr/local/bin/aria2c
+# rm -rf /root/aria2*
+# apt-get autoremove -y
 
-# curl -LO https://github.com/q3aql/aria2-static-builds/releases/download/v1.36.0/aria2-1.36.0-linux-gnu-64bit-build1.tar.bz2
-# tar jxvf aria2-1.36.0-linux-gnu*.bz2
-# rm ria2-1.36.0-linux-gnu*.bz2
-# cd aria2-1.36.0-linux*
-# sudo make install
+curl -LO https://github.com/abcfy2/aria2-static-build/releases/download/1.37.0/aria2-x86_64-linux-musl_static.zip
+unzip aria2-x86_64-linux-musl_static.zip
+rm aria2-x86_64-linux-musl_static.zip
+mv aria2c /usr/bin/aria2c
 
   cat > '/etc/systemd/system/aria2.service' << EOF
 [Unit]
